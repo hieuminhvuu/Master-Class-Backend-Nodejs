61bdf335fc049765b98a19e6ca8d875e413d4631
Hieu Minh Vu <hieuminhvuu@AlienBookpro.local>
2023-05-06 18:17:46 +0300

    Init project

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const compression = require("compression");
+const app = express();
+
+// init middleware
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(compression());
+
+// init db
+
+// init routes
+app.get("/", (req, res, next) => {
+    return res.status(200).json({
+        message: "Well well well",
+    });
+});
+
+// handling error
+
+module.exports = app;
