e1ef2378187e38c36e4d4146d35fac3af4357a95
Hieu Minh Vu <hieuminhvuu@AlienBookpro.local>
2023-05-22 18:13:36 +0300

    api key - permission

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,15 @@
 "use strict";
 
 const express = require("express");
+const { apiKey, permission } = require("../auth/checkAuth");
 const router = express.Router();
 
+// Check api key
+router.use(apiKey);
+
+// Check permission
+router.use(permission("0000"));
+
 router.use("/v1/api", require("./access"));
-// router.get("", (req, res, next) => {
-//     return res.status(200).json({
-//         message: "Welcome to the league of Draven)",
-//     });
-// });
 
 module.exports = router;
